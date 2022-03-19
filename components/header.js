3e6762caae5383b154dc15029233760fdbc650b5
Alex Berkuta <swe3tpandemouim@gmail.com>
2022-03-19 09:45:05 +0200

    added new features

--- components/header.js
+++ components/header.js
@@ -1,32 +1,41 @@
 import styles from '../styles/Home.module.css'
 import { SocialIcon } from 'react-social-icons'
 import Head from 'next/head'
+
+
 function Header() {
 return(<div>
 <Head>
+
+
+
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Marck+Script&display=swap" rel="stylesheet"/> 
 <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet"/> 
 </Head>
 
- <header className={styles.gridZip}>
-    <div>
-    <img src="/norlogo.png" className={styles.headPhoto}/>
+ <header >
+     
+ <div className={styles.gridZip}>
+ <div className={styles.headPhoto}>
+    <img src="/norlogo.png" />
     </div>
     <div className={styles.cards}> 
     <ul className={styles.headerul}>    
-        <li className={styles.li}><a href="/" >
+        <li className={styles.li}    
+        ><a href="/" >
         <h2>Home</h2>
         </a></li>
-        <li className={styles.li}><a href="/about" >
+        <li className={styles.li}   
+        ><a href="/about" >
         <h2>About</h2>
         </a></li>
-        <li className={styles.li}><a href="/works" >
-        <h2>My Works</h2>
+        <li className={styles.li} 
+        ><a href="/works" >
+        <h2>Gallery</h2>
         </a></li>
-        <li className={styles.li}><a href="/price" >
-        <h2>Price list</h2>
-        </a></li>
+        
         <li className={styles.li}>
         <a href='https://instagram.com/just.renaissance.tattoo/' >
         <SocialIcon url='https://instagram.com/just.renaissance.tattoo/' />
@@ -37,6 +46,8 @@ return(<div>
         </li>
     </ul>    
     </div>
+ </div>
+
  </header>
 </div>
                         
--- pages/location.js
+++ pages/location.js
@@ -1,15 +1,42 @@
 import styles from '../styles/Home.module.css'
 
-function Location(){
+import { SocialIcon } from 'react-social-icons'
+
+const Footer = (props) => {
     return (
-        <>
-        <img className={styles.footPhoto}
-        src="/public/Location.jpg" 
-        />
-        </>
-    )
+    <>
     
+    <div className={styles.cards}> 
+    <ul className={styles.footerul}>    
+        <li className={styles.li}
+        
+        ><a href="/" >
+        <h2>Home</h2>
+        </a></li>
+        <li className={styles.li}
+         
+        ><a href="/about" >
+        <h2>About</h2>
+        </a></li>
+        <li className={styles.li}
+         
+        ><a href="/works" >
+        <h2>Gallery</h2>
+        
+        </a></li>
+       
+        <li className={styles.li}>
+        <a href='https://instagram.com/just.renaissance.tattoo/' >
+        <SocialIcon url='https://instagram.com/just.renaissance.tattoo/' />
+        </a>          
+        <a href='https://t.me/lord_of_ink_and_needles' >
+        <SocialIcon url='https://telegram.me/lord_of_ink_and_needles' />
+        </a>
+        </li>
+    </ul>    
+    </div>
+    </>
+    );
 }
 
-
-export default Location
+export default Footer;
